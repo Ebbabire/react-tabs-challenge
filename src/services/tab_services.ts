c0d2a3fca6e553920d06682348ec4c27d555067a
Ebba Birhanu <ebba.birhanu@gmail.com>
2024-10-28 10:14:20 +0300

    added a `.env` file and a proper import statment for the base url

--- src/services/tab_services.ts
+++ src/services/tab_services.ts
@@ -1,6 +1,6 @@
 // function that fetches dummy post data form jsonplaceholder
 export const getTabData = async (key: string): Promise<string> => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${key}`);
+  const res = await fetch(`${import.meta.env.VITE_BASE_URL}/posts/${key}`);
   if (!res.ok) {
     throw new Error("Network response was not ok");
   }
